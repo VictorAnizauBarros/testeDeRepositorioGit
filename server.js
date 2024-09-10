6e08af6540210572ffa8a1eb36a2554271dc5194
VICTOR HUGO ANIZAU BARROS <victor.barros43@etec.sp.gov.br>
2024-09-10 20:01:34 -0300

    Iniciando o meu projeto

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+const app = express(); 
+const port = 3000; 
+
+app.use(express.json()); 
+
+app.get(); 
+
+app.put(); 
+
+app.post; 
+
+app.delete()
+
+app.listen(port, ()=>{
+    console.log('server is runing...')
+})
